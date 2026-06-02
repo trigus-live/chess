6661672b57ed3f94e0a85016b953e5ba7defcca1
trigus <a@b.c>
2026-06-02 17:21:10 +0200

    implemented full move generation for pawns

--- src/main/java/me/trigus/chess/game/Piece.java
+++ src/main/java/me/trigus/chess/game/Piece.java
@@ -1,5 +1,7 @@
 package me.trigus.chess.game;
 
+import me.trigus.chess.util.Bitmask;
+
 public class Piece {
 
     private final PieceType pieceType;
@@ -11,6 +13,47 @@ public class Piece {
     }
     public Piece(PieceType pieceType) {
         this (pieceType, 0L);
+    }
+
+    public long getLegalMoves (GameState gameState) {
+        long rawMoves = pieceType.getRawMoves(position);
+        long bitmaskAllPieces = gameState.getBitBoardAllPieces();
+
+        if (pieceType == PieceType.PAWN || pieceType == PieceType.PAWN_B) {
+            // pawns can't capture pieces directly
+            rawMoves &= ~bitmaskAllPieces;
+
+            // pawns can't double push if a piece is in the way
+            if (pieceType.isWhite && (position & Bitmask.rank(1).mask()) != 0) {
+                Piece front = gameState.getPiece(position << 8);
+                if (front != null) rawMoves &= ~Bitmask.rank(3).mask();
+
+            } else if (!pieceType.isWhite && (position & Bitmask.rank(6).mask()) != 0) {
+                Piece front = gameState.getPiece(position >>> 8);
+                if (front != null)
+                    rawMoves &= ~Bitmask.rank(4).mask();
+            }
+
+            // add pawn captures including en-passant
+            long pawnCaptureMask = 0L;
+            if (pieceType.isWhite) {
+                pawnCaptureMask |= position << 7;
+                pawnCaptureMask |= position << 9;
+            } else {
+                pawnCaptureMask |= position >>> 7;
+                pawnCaptureMask |= position >>> 9;
+            }
+
+            // filter board wraps
+            if ((position & Bitmask.file(0).mask()) != 0x0L) pawnCaptureMask &= ~Bitmask.file(7).mask();
+            if ((position & Bitmask.file(7).mask()) != 0x0L) pawnCaptureMask &= ~Bitmask.file(0).mask();
+
+            rawMoves |= pawnCaptureMask & (gameState.getBitboardPieces(!getPieceType().isWhite) | gameState.getEnPassantPosition());
+        }
+
+
+
+        return rawMoves;
     }
 
     public long getPosition() {
